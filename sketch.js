313f0989919b0af3e7e9617040dfae309e5f3719
Oscar Hafner <hafner.oscar@gmail.com>
2020-12-17 20:26:20 +0100

    + Update sketch

--- sketch.js
+++ sketch.js
@@ -1,46 +1,46 @@
-const colorBlanco = 255;
-const colorNegro = 0;
+const CANVAS_WIDTH = 400;
+const CANVAS_HEIGHT = 400;
 
-const tamanoCelda = 40;
-const numeroDeCeldas =8;
-const celdaInicial = 0;
-
-var colorInicial = colorBlanco;
-
-function filaTablero(posicionY, colorPrimeraCasilla) {
-  var colorPincel = colorPrimeraCasilla;
-  for(casillaX = celdaInicial; casillaX < numeroDeCeldas; casillaX += 1) {
-    fill(colorPincel)
-    rect(20 + tamanoCelda*casillaX, posicionY,  tamanoCelda, tamanoCelda)    
-    colorPincel = seleccionaColorPincel(colorPincel)
-  }
-}
-// seleccionaColorPincel(colorBlanco) ---> colorNegro
-// seleccionaColorPincel(colorNegro) ---> colorBlanco
-function seleccionaColorPincel(colorActual) {
-  var colorNuevo;
-  // En cada ciclo copruebo el color del pincel y 
-    // voy intercambiandolo
-    if (colorActual === colorBlanco) { 
-      // Asigna color negro al color del pincel
-      colorNuevo = colorNegro; 
-    }
-    else { 
-      // Asigna color blanco al color del pincel
-      colorNuevo = colorBlanco; 
-    }
-  return colorNuevo;
-}
+// Posición inicial de nuestro tablero 
+let x0 = 100;
+let y0 = 100;
+// Tamaño de una celda
+let sizeCell = 20
+// Numero de celdas 
+let numCellsX = 8;
+let numCellsY = 8;
+// Color de la celda (iniciamos en 0 - Negro)
+let cellColor = 0;
 
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
 }
-      // i++ => i + 1 | i-- => i - 1
-      // quad
-function draw() {
-  var colorInicial = colorBlanco;
-  for(filaY = 0; filaY < numeroDeCeldas; filaY++) {
-    filaTablero(20 + tamanoCelda*filaY, colorInicial);
-    colorInicial = seleccionaColorPincel(colorInicial)
+
+function flipColor(value) {
+  if (value === 0) { return 255; }
+  else { return 0; }
+}
+
+function drawChessRow(X0, Y0, firstCellColor) {
+  // Dibuja 8 cuadrados
+  for (let cellX = 0; cellX < numCellsX; cellX += 1) {
+   fill(firstCellColor); // Relleno (negro/blanco)
+   /*
+   Utiliza el valor del bucle for para desplazar el cuadrado a la derecha
+   Y0 se mantiene constante durante todo el bucle
+   */
+   rect(X0 + sizeCell*cellX , Y0, sizeCell, sizeCell)
+   firstCellColor = flipColor(firstCellColor) // Cambio el color para el siguiente cuadrado
   }  
+}
+
+function draw() {
+ // Dibuja 8 filas
+ for (let rowY = 0; rowY < numCellsY; rowY++) {
+   // En cada iteración, desplaza la posición inicial de la fila en sizeCell pixeles.
+   drawChessRow(x0, y0 + sizeCell * rowY, cellColor);
+   // En cada iteración, cambia el color con el que comienza.
+   cellColor = flipColor(cellColor);
+ }
+ sizeCell = mouseX / width * 100;
 }
